14317578a31d28b076eb9427464f8420bce015f5
Henrique Moriggi Justino <henrique.justino@educacao.sp.gov.br>
2023-08-23 08:59:20 -0300

    change profile image and readme

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function toggleMode() {
   //const alt = document.querySelector("#profile alt")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/Senku-light-mode.jpg")
     img.setAttribute("alt", "Foto do usuário light mode")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Senku-dark-mode.jpg")
     img.setAttribute("alt", "Foto do usuário dark mode")
   }
 }
